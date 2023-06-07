71f868461edd9a86e6f472f9e4286b54ceec749a
Kevin Dietz <kdietz65@gmail.com>
2023-06-06 23:57:10 -0500

    Basic script to download zip and run setup

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,1 +1,7 @@
-echo "Hello. This is a test."
+apt update
+apt install unzip
+cd /opt
+wget http://files.opensquiggly.com/opensquiggly.latest.zip
+unzip opensquiggly.latest.zip -d OpenSquiggly
+cd /opt/OpenSquiggly/setup
+bash linode-setup.sh
